1d15cb9694743421096e59cf6beace9cdcbb76c7
Lim Yew Hao <lim_yew_hao@a-star.edu.sg>
2026-05-26 14:52:41 +0800

    feat: add master-side traversability, action-conflict, mode, and factsheet validators
    Signed-off-by: Lim Yew Hao <lim_yew_hao@a-star.edu.sg>

--- vda5050_core/include/vda5050_core/errors/error_codes.hpp
+++ vda5050_core/include/vda5050_core/errors/error_codes.hpp
@@ -31,6 +31,12 @@ inline const std::string OrderUpdateError = "orderUpdateError";
 inline const std::string ValidationError = "validationError";
 inline const std::string ContentValidationError = "contentValidationError";
 inline const std::string PreSendValidationError = "preSendValidationError";
+inline const std::string TraversabilityValidationError =
+  "traversabilityValidationError";
+inline const std::string ActionBlockedByDrivingError =
+  "actionBlockedByDrivingError";
+inline const std::string HardActionBlockedError = "hardActionBlockedError";
+inline const std::string ModeValidationError = "modeValidationError";
 
 // Reference key string for vda5050_types::ErrorReference::key
 inline const std::string RefOrderId = "orderId";
@@ -40,6 +46,7 @@ inline const std::string RefEdgeId = "edgeId";
 inline const std::string RefActionId = "actionId";
 inline const std::string RefActionType = "actionType";
 inline const std::string RefSequenceId = "sequenceId";
+inline const std::string RefActionId = "actionId";
 
 }  // namespace errors
 }  // namespace vda5050_core
--- vda5050_core/include/vda5050_core/validation/pre_send_validator.hpp
+++ vda5050_core/include/vda5050_core/validation/pre_send_validator.hpp
@@ -22,8 +22,10 @@
 #include <optional>
 
 #include "vda5050_core/errors/validation_result.hpp"
+#include "vda5050_core/layout/graph.hpp"
 #include "vda5050_core/master/agv.hpp"
 #include "vda5050_core/types/connection_state.hpp"
+#include "vda5050_core/types/factsheet.hpp"
 #include "vda5050_core/types/state.hpp"
 
 namespace vda5050_core::validation {
@@ -33,7 +35,9 @@ struct PreSendContext
 {
   vda5050_core::types::ConnectionState connection_status;
   std::optional<vda5050_core::types::State> last_state;
+  std::optional<vda5050_core::types::Factsheet> last_factsheet;
   vda5050_core::master::AGVState operational_state;
+  vda5050_core::layout::Graph::ConstPtr loaded_graph;
 };
 
 /// \brief AGV-readiness gate before publish (connection, mode, position).

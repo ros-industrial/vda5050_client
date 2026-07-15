593c8d3a5b2c776deefba9d8ac7f1677957b2588
Saurabh Kamat <saurabh_kamat@a-star.edu.sg>
2026-07-15 17:37:56 +0800

    test: add a test fixture to configure adapter
    Signed-off-by: Saurabh Kamat <saurabh_kamat@a-star.edu.sg>

---
+++ vda5050_core/test/client/adapter_test_fixture.hpp
@@ -0,0 +1,92 @@
+/*
+ * Copyright (C) 2025 ROS-Industrial Consortium Asia Pacific
+ * Advanced Remanufacturing and Technology Centre
+ * A*STAR Research Entities (Co. Registration No. 199702110H)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef CLIENT__ADAPTER_TEST_FIXTURE_HPP_
+#define CLIENT__ADAPTER_TEST_FIXTURE_HPP_
+
+#include <chrono>
+#include <memory>
+#include <string>
+#include <thread>
+#include <unordered_map>
+#include <utility>
+#include <vector>
+
+#include "vda5050_core/client/adapter/adapter.hpp"
+#include "vda5050_core/execution/protocol_adapter.hpp"
+
+#include "mock_mqtt_client.hpp"
+
+using namespace vda5050_core::client::adapter;  // NOLINT
+using namespace vda5050_core::execution;        // NOLINT
+using namespace vda5050_core::transport;        // NOLINT
+using namespace testing;                        // NOLINT
+
+class AdapterTest : public testing::Test
+{
+protected:
+  void SetUp() override
+  {
+    mqtt = std::make_shared<NiceMock<MockMqttClient>>();
+
+    ON_CALL(*mqtt, connected()).WillByDefault(Return(true));
+
+    ON_CALL(*mqtt, subscribe(_, _, _))
+      .WillByDefault(
+        [this](
+          const std::string& topic, MqttClientInterface::MessageHandler handler,
+          int /*qos*/) { subscriptions[topic] = std::move(handler); });
+
+    ON_CALL(*mqtt, publish(_, _, _, _))
+      .WillByDefault(
+        [this](
+          const std::string& topic, const std::string& message, int /*qos*/,
+          bool /*retain*/) { published.push_back({topic, message}); });
+
+    protocol_adapter =
+      ProtocolAdapter::make(mqtt, "uagv", "2.0.0", "Robot", "001");
+
+    adapter = Adapter::make(protocol_adapter);
+  }
+
+  bool wait_publish(size_t publish_count)
+  {
+    while (published.size() < publish_count)
+    {
+      std::this_thread::sleep_for(std::chrono::milliseconds(5));
+    }
+    return true;
+  }
+
+  struct PublishedMessage
+  {
+    std::string topic;
+    std::string message;
+  };
+
+  std::shared_ptr<NiceMock<MockMqttClient>> mqtt;
+  std::shared_ptr<vda5050_core::execution::ProtocolAdapter> protocol_adapter;
+  std::shared_ptr<Adapter> adapter;
+
+  std::unordered_map<std::string, MqttClientInterface::MessageHandler>
+    subscriptions;
+
+  std::vector<PublishedMessage> published;
+};
+
+#endif  // CLIENT__ADAPTER_TEST_FIXTURE_HPP_
